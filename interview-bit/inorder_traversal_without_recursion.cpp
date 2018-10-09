c83e1d261d75df6596b9aa589749251330697695
shivanand217 <shivakp2111@gmail.com>
2018-10-09 21:38:12 +0530

    problem-2

--- interview-bit/inorder_traversal_without_recursion.cpp
+++ interview-bit/inorder_traversal_without_recursion.cpp
@@ -7,7 +7,31 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+#include<iostream>
+
 vector<int> Solution::inorderTraversal(TreeNode* A) {
 
-    
+    vector<int> ans;
+    stack<int> my_stack;
+    TreeNode *root = A;
+    TreeNode *current = A->left;
+    TreeNode *top;
+
+    my_stack.push(root);
+    while(!my_stack.empty()) {
+        
+        if(current == NULL) {
+            top = my_stack.top();
+            my_stack.pop();
+            ans.push_back(top->val);
+            current = current->right;
+
+        } else {
+            my_stack.push(current);
+            current = current->left;
+        }
+    }
+
+    return ans;
 }
